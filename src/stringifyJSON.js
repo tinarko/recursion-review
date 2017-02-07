5a7e7e4229d065c3128c46d971f43cc529c52d63
tinarko <tinarchan@gmail.com>
2017-02-06 16:00:52 -0800

    pass tests up to stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //debugger;
+  var string = '';
+
+  if (obj === null) {
+    string += 'null';
+  } else if (typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+    string += obj;
+  } else if (typeof(obj) === 'string') {
+    string += '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    string += '[';
+    var arrString = [];
+    for (var i = 0; i < obj.length; i++) {
+      arrString.push(stringifyJSON(obj[i]));
+    }
+    string += arrString.join(',');
+    string += ']';
+  } else if (typeof(obj) === 'object') {
+    string += '{';
+    var arrString = [];
+    for (var key in obj) {
+      if (typeof(obj[key]) !== 'function' && typeof(obj[key]) !== 'undefined') {
+        arrString.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    string += arrString.join(',');
+    string += '}';
+  }
+  return string;
 };
